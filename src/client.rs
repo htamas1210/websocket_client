39eb1b7cd31fe332a664d6b0dae840fd33012917
htom <hatvanitamas09@gmail.com>
2025-11-12 13:01:27 +0100

    new uid in every message

--- src/client.rs
+++ src/client.rs
@@ -2,10 +2,12 @@ use futures_util::{SinkExt, StreamExt};
 use serde::{Deserialize, Serialize};
 use tokio_tungstenite::connect_async;
 use url::Url;
+use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Message {
     username: String,
+    uid: Uuid,
     text: String,
 }
 
@@ -14,12 +16,13 @@ async fn main() {
     let url = Url::parse("ws://127.0.0.1:9001").unwrap();
     let (mut socket, _) = connect_async(url).await.expect("Can't connect");
 
-    let msg = Message {
-        username: "Alice".into(),
-        text: "Hello, server!".into(),
-    };
-
     loop {
+        let msg = Message {
+            username: "Alice32".into(),
+            uid: Uuid::new_v4(),
+            text: "Hello, server!".into(),
+        };
+
         let serialized = serde_json::to_string(&msg).unwrap();
         socket
             .send(tokio_tungstenite::tungstenite::Message::Text(serialized))
